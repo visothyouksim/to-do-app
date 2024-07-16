d29cdc749703281235af6e7fafdba9ec04430d14
Visoth YOUK-SIM <115548934+visothyouksim@users.noreply.github.com>
2024-07-16 09:24:27 +0200

    ajout OK, toDo to Finished OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+interface Task {
+  name: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  newTask: string = '';
+  tasks: Task[] = [
+    { name: 'Faire du café', completed: false },
+    { name: 'Faire du thé', completed: false },
+    { name: 'Faire du riz', completed: false },
+    { name: 'Faire des pâtes', completed: false },
+    { name: 'Faire la vaisselle', completed: false }
+  ];
+
+  completedTasks: Task[] = [];
+
+  addTask() {
+    if (this.newTask.trim().length > 0) {
+      this.tasks.push({ name: this.newTask, completed: false });
+      this.newTask = '';
+    }
+  }
+
+  toggleTask(task: Task) {
+    task.completed = !task.completed;
+    if (task.completed) {
+      this.completedTasks.push(task);
+      this.tasks = this.tasks.filter(t => t !== task);
+    } else {
+      this.tasks.push(task);
+      this.completedTasks = this.completedTasks.filter(t => t !== task);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
